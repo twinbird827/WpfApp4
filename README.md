a06df9dc0edc95d42364fc75a5b56202e520f6eb
twinbird827 <twinbird827@gmail.com>
2018-06-07 09:40:24 +0900

    commit

---
+++ README.md
@@ -0,0 +1,29 @@
+WpfApp4
+====
+
+## Overview
+
+ContextMenuをenumによって切り替えられるか確認  
+結論：DataTriggerで切り替えられる
+
+## Description
+
+## Demo
+
+## VS. 
+
+## Requirement
+
+## Usage
+
+## Install
+
+## Contribution
+
+## Licence
+
+[MIT](https://github.com/twinbird827/WpfApp4/blob/master/LICENSE)
+
+## Author
+
+[twinbird827](https://github.com/twinbird827)
---
+++ ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4
+{
+    class ViewModel : BindableBase
+    {
+        public string Text
+        {
+            get { return _Text; }
+            set { SetProperty(ref _Text, value); }
+        }
+        private string _Text;
+
+        public MenuType Type
+        {
+            get { return _Type; }
+            set { SetProperty(ref _Type, value); }
+        }
+        private MenuType _Type = MenuType.AAA;
+
+    }
+}
